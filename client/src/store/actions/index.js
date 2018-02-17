03b753b575d9477547550dcc2ac27a70df92d1e0
Benjamin Ross <benjaminross1001@gmail.com>
2018-02-17 15:36:24 -0700

    add action types actions for signup and signin and corresponding reducers

---
+++ client/src/store/actions/index.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import { BrowserRouter } from "react-router-dom";
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER, FETCH_MESSAGE } from "./types";
+
+const ROOT_URL = "http://localhost:3090";
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    axios
+      .post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+
+        localStorage.setItem("token", response.data.token);
+
+        BrowserRouter.push("/feature");
+      })
+      .catch(error => {
+        dispatch(authError("Bad Login Info"));
+      });
+  };
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios
+      .post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+
+        localStorage.setItem("token", response.data.token);
+
+        BrowserRouter.push("/feature");
+      })
+      .catch(error => {
+        dispatch(authError(error.response.data.error));
+      });
+  };
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
+}
+
+export function signoutUser() {
+  localStorage.removeItem("token");
+  return {
+    type: UNAUTH_USER
+  };
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios
+      .get(ROOT_URL, {
+        headers: { authorization: localStorage.getItem("token") }
+      })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        });
+      });
+  };
+}
