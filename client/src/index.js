0ffb3c2921acbf51c5a493a898e6198ceaebfcae
Benjamin Ross <benjaminross1001@gmail.com>
2018-02-17 15:36:51 -0700

    incorporate redux and react router dom into index.js component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import reducers from "./store/reducers";
+import { AUTH_USER } from "./store/actions/types";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Signin from "./components/Signin/Signin";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const createStoreWithMiddleware = composeEnhancers(applyMiddleware(reduxThunk))(
+  createStore
+);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem("token");
+
+if (token) {
+  store.dispatch({ type: AUTH_USER });
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
