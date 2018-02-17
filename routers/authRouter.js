dfeb222b36861cdd917989b96e63dd278cf1a657
squeemishly <squeemishly@gmail.com>
2018-02-17 11:19:36 -0700

    create authentication route
    add engines

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,5 +1,9 @@
+const Authentication = require('../library/controllers/AuthController')
+
 module.exports = app => {
   app.get('/', (req, res) => {
     res.send({squee: "awesome"})
   })
+
+  app.post('/signup', Authentication.signup)
 }
