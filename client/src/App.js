cdea620867a8b17d117f7b41f8acf69fb3e97707
Padraic O'Neill <pconeill@gmail.com>
2018-02-17 10:24:44 -0700

    creates initial client react app, adds babel, adds proxy for port 8080 (#1)
    * create initial client react app, adds babel, adds proxy for port 8080
    
    * add boiler truffle, removes some extraneous react boilerplate files
    
    * add a comments as placeholder for truffle integration
    
    * add web3js file as utility
    
    * update package json
    
    * run yarn eject, server runs without errors

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import contracts from build json files
+// import getWeb3 from './utils/getWeb3'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Dappl Vote</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
