1cafbb29097e2eed94fd82c3e904cafeb1c3d8cf
Padraic O'Neill <pconeill@gmail.com [core]>
2018-02-21 12:52:57 -0700

    add createVoter to migration. successfully creates a voter during migration.

--- client/migrations/2_deploy_contracts.js
+++ client/migrations/2_deploy_contracts.js
@@ -6,5 +6,13 @@ const candidateParties = ["GOP", "Democrat"]
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Ownable);
-  deployer.deploy(Ballot, candidateNames, candidateParties, {from: accounts[0]});
+  deployer.deploy(Ballot, candidateNames, candidateParties, {from: accounts[0]})
+    .then(function(instance) {
+      ballot = Ballot.deployed();
+      return ballot;
+    })
+    .then(function(instance) {
+      return instance.createVoter("0xf17f52151ebef6c7334fad080c5704d77216b732", 345321)
+    })
+    .catch(function() { console.log("Error: Contract functions not completed.")})
 };
